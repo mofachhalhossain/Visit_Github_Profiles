934a9aa1d83ab469602f83250bd9574c67772f0f
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-11-04 02:09:36 +0600

    way to final

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ async function getUser(user) {
   const response = await fetch(APIURL + user);
   const responseData = await response.json();
   createUserCard(responseData);
+  getRepos(user);
+}
+async function getRepos(user) {
+    const response = await fetch(APIURL + user + '/repos');
+    const responseData = await response.json();
+    addReposToCard(responseData);
 }
 
 function createUserCard(user) {
@@ -27,10 +33,25 @@ function createUserCard(user) {
             <li><strong>Followers</strong>: ${user.followers}</li>
             <li><strong>Following</strong>: ${user.following}</li>
         </ul>
+        <div class="repos" id="repos">
+
+        </div>
     </div>
     </div>
     `;
     main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+    const reposList = document.getElementById('repos');
+    repos.forEach(repo => {
+        const repoHTML = `
+        <li>
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+        </li>
+        `;
+        reposList.innerHTML += repoHTML;
+    });
 }
 
 form.addEventListener('submit', (e) => {
